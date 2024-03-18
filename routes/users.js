4daac2daec5e0f57ed10666c08870308da9014f7
spraneeth28 <156373777+spraneeth28@users.noreply.github.com>
2024-03-18 18:54:59 -0500

    done

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+let product = 1;
+let factor = 1;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  factor = factor+1;
+  product = product * factor;
+
+  res.send(`Product is : ${product}`);
 });
 
 module.exports = router;
